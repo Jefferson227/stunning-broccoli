03a70c49749b231321966bb7c9c9fa4a9984f013
Jefferson Matheus <jefferson227@gmail.com>
2019-11-10 00:02:30 -0300

    Add details info

--- js/movie.js
+++ js/movie.js
@@ -2,15 +2,18 @@
   let showtimesRadio = document.getElementById("showtimes");
   let detailsRadio = document.getElementById("details");
   let movieShowtimes = document.querySelector(".movie-showtimes");
+  let movieDetails = document.querySelector(".movie-details");
 
   showtimesRadio.addEventListener("click", function() {
     if (showtimesRadio.checked) {
       movieShowtimes.classList.add("u-display-block");
+      movieDetails.classList.remove("u-display-block");
     }
   });
 
   detailsRadio.addEventListener("click", function() {
     if (detailsRadio.checked) {
+      movieDetails.classList.add("u-display-block");
       movieShowtimes.classList.remove("u-display-block");
     }
   });
